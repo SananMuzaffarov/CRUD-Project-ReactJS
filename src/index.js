5d0483f5a1f4a010450c8b9711ec7a323b3c6a00
SananM <smuzefferov1@gmail.com>
2022-12-28 04:57:58 +0400

    created folders

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+const App = () => {
+  return(
+    <div>
+
+    </div>
+  );
+}
+root.render(<App />);
